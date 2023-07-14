f36e1e1d23bcb35b90ed0ad7807bd69e09b398b6
Temoa <temoa_mic@hotmail.com>
2023-07-14 16:55:58 +0800

    添加 Android 例子

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quick_blue_simplify/quick_blue.dart';
+import 'package:quick_blue_simplify_example/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,22 +14,98 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<BlueScanResult> _scanResults = [];
+
   @override
   void initState() {
     super.initState();
+    _onBleScan();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('DEMO'),
         ),
-        body: const Center(
-          child: Text('Running on: '),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  _checkPermission();
+                },
+                child: const Text('搜索'),
+              ),
+              const SizedBox(height: 8),
+              _hostSearchResultWidget(),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Widget _hostSearchResultWidget() {
+    return Expanded(
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: _scanResults.length,
+        itemBuilder: (context, index) {
+          final BlueScanResult scanResult = _scanResults[index];
+          return Card(
+            child: ListTile(
+              title: Text(scanResult.name),
+              subtitle: Text(scanResult.deviceId),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void _onBleScan() {
+    QuickBlue.scanResultStream.listen((result) {
+      final temp = [..._scanResults];
+      temp.add(result);
+      print('look ${result.allManufacturerDataHead}');
+      setState(() {
+        _scanResults = temp;
+      });
+    });
+  }
+
+  void _startBleScan() async {
+    QuickBlue.startScan();
+    Future.delayed(const Duration(seconds: 3), () {
+      _stopBleScan();
+    });
+  }
+
+  void _stopBleScan() async {
+    QuickBlue.stopScan();
+  }
+
+  Future<void> _checkPermission() async {
+    final granted = await PermissionHandler.requestBlePermission();
+    if (!mounted) return;
+    if (granted) {
+      final isLocationEnable = await PermissionHandler.checkLocationIsEnable();
+      if (isLocationEnable) {
+        final isBleEnable = await PermissionHandler.checkBleIsEnable();
+        if (isBleEnable) {
+          _startBleScan();
+        } else {
+          print('需要开启蓝牙');
+        }
+      } else {
+        print('需要开启定位');
+      }
+    } else {
+      print('需要相关权限');
+    }
   }
 }
---
+++ example/lib/permission_handler.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:permission_handler/permission_handler.dart';
+
+class PermissionHandler {
+  static Future<bool> requestBlePermission() async {
+    if (Platform.isAndroid) {
+      return await _requestBlePermissionAndroid();
+    } else {
+      return await _requestBlePermissionIOS();
+    }
+  }
+
+  static Future<bool> _requestBlePermissionIOS() async {
+    final isBleGranted = await Permission.bluetooth.request();
+    return isBleGranted == PermissionStatus.granted;
+  }
+
+  static Future<bool> _requestBlePermissionAndroid() async {
+    final Map<Permission, PermissionStatus> isBleGranted = await [
+      Permission.bluetoothScan,
+      Permission.bluetoothConnect,
+    ].request();
+
+    return isBleGranted[Permission.bluetoothScan] == PermissionStatus.granted && //
+        isBleGranted[Permission.bluetoothConnect] == PermissionStatus.granted;
+  }
+
+  static Future<bool> checkLocationIsEnable() async {
+    return (await Permission.locationWhenInUse.serviceStatus.isEnabled) == true;
+  }
+
+  static Future<bool> checkBleIsEnable() async {
+    return (await Permission.bluetooth.serviceStatus.isEnabled) == true;
+  }
+}
